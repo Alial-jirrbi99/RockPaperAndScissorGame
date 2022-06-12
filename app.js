c5799a6c4036219ff45232acbb98b62cddd9697e
Alial-jirrbi99 <ajirrbi@gmail.com>
2022-06-12 20:19:57 +0300

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// Selecting elements.
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
 const showIcon = document.querySelector('.show i');
 const computerShowIcon = document.querySelector('.computer i');
 
-// Storing the scores.
 let computerScore = 1;
 let playerScore = 1;
 
@@ -38,24 +36,20 @@ const lose = ()=>{
     text2.style.color = 'red';
 }
 
-// Game Functionality.
 const game = () =>{
     buttons.forEach(btn =>{
         btn.addEventListener('click',(e)=>{
-        // Random rock paper scissor for the computer and clicked ones for the player
            let clickedBtn = e.target.className;
            showIcon.className = clickedBtn;
            let randomNum = Math.floor(Math.random() * randomClasses.length);
            computerShowIcon.className = randomClasses[randomNum];
 
-           // If it's a Tie .
            if(showIcon.className === computerShowIcon.className){
                pScore.innerHTML = pScore.innerHTML;
                cScore.innerHTML = cScore.innerHTML;
                tie();
            }
 
-           // if it's not a Tie.
            else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
@@ -85,5 +79,4 @@ const game = () =>{
     });
 }
 
-// Run the game.
 game();
